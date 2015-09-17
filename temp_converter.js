6261fb5921b54b15875e3469e223dbe42251169d
Sean Thorpe <sean.thorpe@west-mec.org>
2015-09-17 13:54:40 -0700

    no message

--- temp_converter.js
+++ temp_converter.js
@@ -2,8 +2,13 @@
 var fahrenheit = 32;
 
 
-var fahrenheitToCelsius = ((fahrenheit - 32) / 1.8);
-var fahrenheitToKelvin = ((fahrenheit + 459.67)/1.8);
+var fahrenheitToCelsius = (fahrenheit - 32) / 1.8;
+var fahrenheitToKelvin = (fahrenheit + 459.67)/1.8;
+
+console.log("Starting with Fahrenheit");
+console.log("Fahrenheit: " + fahrenheit);
+console.log("Celsius: " + fahrenheitToCelsius);
+console.log("Kelvin: " + fahrenheitToKelvin);
 
 
 var celsius = 0;
@@ -12,9 +17,18 @@ var celsius = 0;
 var celsiusToFahrenheit = (celsius * 1.8 + 32);
 var celsiusToKelvin = (celsius + 273.15);
 
+console.log("Starting with Celcius");
+console.log("Celsius: " + celsius);
+console.log("Fahrenheit: " + celsiusToFahrenheit);
+console.log("Kelvin: " + celsiusToKelvin);
 
 var kelvin = 273.15;
 
 
-var kelvinToFahrenheit = (kelvin * 1.8 - 459.67);
+var kelvinToFahrenheit = (kelvin - 273.15) * 1.8 + 32;
 var kelvinToCelsius = (kelvin - 273.15);
+
+console.log("Starting with Kelvin");
+console.log("Kelvin: " + kelvin);
+console.log("Fahrenheit: " + kelvinToFahrenheit);
+console.log("Celcius: " + kelvinToCelsius);
